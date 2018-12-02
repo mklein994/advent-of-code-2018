cb86d945651df51e8dcc56421bd6dd2147be9862
Matthew Klein <mklein994@gmail.com>
2018-12-02 16:36:49 -0600

    Add solution for day 1 with tests

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -1,3 +1,30 @@
 fn main() {
-    println!("Hello, world!");
+    let input = aoc2018::read_file(1);
+    let freq = get_freq(&input);
+
+    println!("{}", freq);
+}
+
+fn get_freq(input: &str) -> i32 {
+    input.lines().map(|c| c.parse::<i32>().unwrap()).sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example_1() {
+        assert_eq!(3, get_freq("+1\n+1\n+1"));
+    }
+
+    #[test]
+    fn test_example_2() {
+        assert_eq!(0, get_freq("+1\n+1\n-2"));
+    }
+
+    #[test]
+    fn test_example_3() {
+        assert_eq!(-6, get_freq("-1\n-2\n-3"));
+    }
 }
