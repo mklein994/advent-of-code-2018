62988e70efd682b3f6165c937144727262d73abd
Matthew Klein <mklein994@gmail.com>
2018-12-02 19:27:57 -0600

    Add solution for day 1 part 2 with tests

--- src/bin/1.rs
+++ src/bin/1.rs
@@ -3,10 +3,28 @@ fn main() {
     let freq = get_freq(&input);
 
     println!("{}", freq);
+
+    let calib_freq = calibrate_freq(&input);
+
+    println!("{}", calib_freq);
 }
 
 fn get_freq(input: &str) -> i32 {
     input.lines().map(|c| c.parse::<i32>().unwrap()).sum()
+}
+
+fn calibrate_freq(input: &str) -> i32 {
+    let mut seen: Vec<i32> = Vec::new();
+    let mut sum = 0;
+    let lines = input.lines().map(|c| c.parse::<i32>().unwrap()).cycle();
+    for i in lines {
+        seen.push(sum);
+        sum += i;
+        if seen.iter().any(|&x| x == sum) {
+            break;
+        }
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -31,5 +49,35 @@ mod tests {
     #[test]
     fn test_part_1() {
         assert_eq!(402, get_freq(include_str!("../../input/1.txt")));
+    }
+
+    #[test]
+    fn part_2_example() {
+        assert_eq!(2, calibrate_freq("+1\n-2\n+3\n+1"));
+    }
+
+    #[test]
+    fn part_2_test_1() {
+        assert_eq!(0, calibrate_freq("+1\n-1"));
+    }
+
+    #[test]
+    fn part_2_test_2() {
+        assert_eq!(10, calibrate_freq("+3\n+3\n+4\n-2\n-4"));
+    }
+
+    #[test]
+    fn part_2_test_3() {
+        assert_eq!(5, calibrate_freq("-6\n+3\n+8\n+5\n-6"));
+    }
+
+    #[test]
+    fn part_2_test_4() {
+        assert_eq!(14, calibrate_freq("+7\n+7\n-2\n-7\n-4"))
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(481, calibrate_freq(include_str!("../../input/1.txt")));
     }
 }
