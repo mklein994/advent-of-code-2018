47bc5cb7e0e3c499760588f7ba3b414a57c762f4
Matthew Klein <mklein994@gmail.com>
2018-12-02 22:57:10 -0600

    WIP: attempt to start day 2

--- src/bin/2.rs
+++ src/bin/2.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Day 2");
+    let input = aoc2018::read_file(2);
+
+    let hash = checksum(&input);
+    println!("{}", hash);
+}
+
+fn checksum(input: &str) -> u32 {
+    let mut two_count: u32 = 0;
+    let mut three_count: u32 = 0;
+
+    for line in input.lines().map(|c| c.chars()) {
+        let (a, b): (u32, u32) = line.fold((0, 0), |count: (u32, u32), i: char| {
+            count
+        });
+
+        two_count += a;
+        three_count += b;
+    }
+
+    two_count * three_count
 }
