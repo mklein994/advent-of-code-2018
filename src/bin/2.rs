d398beeca5fc69ccaedabbeee6988c8ada04b7a5
Matthew Klein <mklein994@gmail.com>
2018-12-03 23:05:27 -0600

    Add solution for day 2 part 1

--- src/bin/2.rs
+++ src/bin/2.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::BTreeMap;
 
 fn main() {
     let input = aoc2018::read_file(2);
@@ -22,16 +22,48 @@ fn checksum(input: &str) -> u32 {
     let mut two_count: u32 = 0;
     let mut three_count: u32 = 0;
 
-    for line in input.lines().map(|c| c.chars()) {
-        let (a, b): (u32, u32) = line.fold((0, 0), |count: (u32, u32), i: char| {
-            count
-        });
+    for line in input.lines() {
+        let mut count = BTreeMap::new();
 
-        two_count += a;
-        three_count += b;
+        for c in line.chars() {
+            let mut entry = count.entry(c).or_insert_with(Occurrence::default);
+
+            if entry.count == 1 && !entry.two {
+                entry.two = true;
+            }
+
+            if entry.count == 2 && entry.two && !entry.three {
+                entry.two = false;
+                entry.three = true;
+            }
+
+            entry.count += 1;
+        }
+
+        let mut two_counted = false;
+        let mut three_counted = false;
+
+        count.iter().for_each(|(_, o)| {
+            if !two_counted && o.two {
+                two_count += 1;
+                two_counted = true;
+            }
+
+            if !three_counted && o.three {
+                three_count += 1;
+                three_counted = true;
+            }
+        });
     }
 
     two_count * three_count
+}
+
+#[derive(Debug, Default)]
+struct Occurrence {
+    count: u32,
+    three: bool,
+    two: bool,
 }
 
 #[cfg(test)]
@@ -49,5 +81,11 @@ aabcdd
 abcdee
 ababab";
         assert_eq!(12, checksum(&input));
+    }
+
+    #[test]
+    fn part_1_test_input() {
+        let input = include_str!("../../input/2.txt");
+        assert_eq!(5229, checksum(&input));
     }
 }
