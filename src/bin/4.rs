e6b0450ec985aa662b5bfbc875b2db5e8e8c0a53
Matthew Klein <mklein994@gmail.com>
2018-12-08 16:32:48 -0600

    Setup Guard struct and supporting structs
    Stub out FromStr for Guard as well.

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 fn main() {
     let input = aoc2018::read_file(4);
     println!("{}", input);
@@ -9,6 +11,57 @@ fn main() {
 
 fn part_1(input: &str) -> u32 {
     unimplemented!()
+}
+
+fn parse_guards(input: &str) -> Result<Vec<Guard>, Box<dyn std::error::Error>> {
+    let mut guards: Vec<Guard> = Vec::with_capacity(input.lines().count());
+
+    for line in input.lines() {
+        guards.push(Guard::from_str(line)?);
+    }
+
+    Ok(guards)
+}
+
+#[derive(Debug)]
+struct Guard {
+    id: u32,
+    sleep_time: Vec<SleepTime>,
+}
+
+#[derive(Debug)]
+struct SleepTime {
+    time: DateTime,
+    state: SleepKind,
+}
+
+#[derive(Debug)]
+struct DateTime {
+    year: u32,
+    month: u32,
+    day: u32,
+    hour: u32,
+    minute: u32,
+}
+
+#[derive(Debug)]
+enum SleepKind {
+    Awake,
+    Asleep,
+}
+
+impl Default for SleepKind {
+    fn default() -> Self {
+        SleepKind::Awake
+    }
+}
+
+impl FromStr for Guard {
+    type Err = Box<dyn std::error::Error>;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        unimplemented!()
+    }
 }
 
 #[cfg(test)]
