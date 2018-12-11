422b4a03f684ed03c1563062a69a5d8874409ca9
Matthew Klein <mklein994@gmail.com>
2018-12-11 16:59:23 -0600

    Create a test for day 5 part 1

--- src/bin/5.rs
+++ src/bin/5.rs
@@ -1,4 +1,30 @@
 fn main() {
     let input = aoc2018::read_file(5);
     println!("{}", input);
+
+    let units = polymer_units(&input);
+    println!("{}", units);
+}
+
+fn polymer_units(input: &str) -> u32 {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1_example() {
+        let input = "dabAcCaCBAcCcaDA";
+
+        assert_eq!(10, polymer_units(input));
+    }
+
+    #[test]
+    #[ignore]
+    fn part_1_test_input() {
+        let input = include_str!("../../input/5.txt");
+        assert_eq!(0, polymer_units(&input));
+    }
 }
