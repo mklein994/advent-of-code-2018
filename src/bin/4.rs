fe7e7e85f0e9146852130cae38f11d476ac914d6
Matthew Klein <mklein994@gmail.com>
2018-12-11 16:39:10 -0600

    Use test input when running day 5

--- src/bin/4.rs
+++ src/bin/4.rs
@@ -6,7 +6,25 @@ use std::ops::Range;
 use std::str::FromStr;
 
 fn main() {
-    let input = aoc2018::read_file(4);
+    // let input = aoc2018::read_file(4);
+    let input = "\
+[1518-11-01 00:00] Guard #10 begins shift
+[1518-11-01 00:05] falls asleep
+[1518-11-01 00:25] wakes up
+[1518-11-01 00:30] falls asleep
+[1518-11-01 00:55] wakes up
+[1518-11-01 23:58] Guard #99 begins shift
+[1518-11-02 00:40] falls asleep
+[1518-11-02 00:50] wakes up
+[1518-11-03 00:05] Guard #10 begins shift
+[1518-11-03 00:24] falls asleep
+[1518-11-03 00:29] wakes up
+[1518-11-04 00:02] Guard #99 begins shift
+[1518-11-04 00:36] falls asleep
+[1518-11-04 00:46] wakes up
+[1518-11-05 00:03] Guard #99 begins shift
+[1518-11-05 00:45] falls asleep
+[1518-11-05 00:55] wakes up";
 
     let part_1_answer = part_1(&input);
 
