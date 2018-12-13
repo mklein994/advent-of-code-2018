7bb30021ef598d08b1bed7722af3bf0c72dccb8f
Matthew Klein <mklein994@gmail.com>
2018-12-13 15:13:40 -0600

    Add test for day 7 part 1

--- src/bin/7.rs
+++ src/bin/7.rs
@@ -1,4 +1,35 @@
 fn main() {
-    let input = aoc2018::read_file(7);
+    // let input = aoc2018::read_file(7);
+    let input = "\
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+
     println!("{}", input);
+}
+
+fn part1(input: &str) -> &str {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_1_example() {
+        let input = "\
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.";
+        assert_eq!("CABDFE", part1(&input));
+    }
 }
