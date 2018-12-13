685bdf0438ccc700039ac8d4542f01c3a072ecdd
Matthew Klein <mklein994@gmail.com>
2018-12-12 19:42:23 -0600

    Setup a test for day 6 part 1

--- src/bin/6.rs
+++ src/bin/6.rs
@@ -2,3 +2,25 @@ fn main() {
     let input = aoc2018::read_file(6);
     println!("{}", input);
 }
+
+fn part1(input: &str) -> usize {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "\
+1, 1
+1, 6
+8, 3
+3, 4
+5, 5
+8, 9";
+
+    #[test]
+    fn part1_example() {
+        assert_eq!(17, part1(&EXAMPLE_INPUT));
+    }
+}
