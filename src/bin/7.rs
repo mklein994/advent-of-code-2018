76a5a8afa81645964b20b61303512b0c496e4da0
Matthew Klein <mklein994@gmail.com>
2018-12-13 16:32:05 -0600

    Parse each line as an instruction

--- src/bin/7.rs
+++ src/bin/7.rs
@@ -1,3 +1,7 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::str::FromStr;
+
 fn main() {
     // let input = aoc2018::read_file(7);
     let input = "\
@@ -8,12 +12,41 @@ Step A must be finished before step D can begin.
 Step B must be finished before step E can begin.
 Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.";
+    // println!("{}", input);
 
-    println!("{}", input);
+    let step_order = part1(&input);
+    println!("{}", step_order);
 }
 
 fn part1(input: &str) -> &str {
+    let steps = input
+        .lines()
+        .map(|line| {
+            line.parse::<Instruction>()
+                .expect("Couldn't parse instruction")
+        })
+        .collect::<Vec<_>>();
+    steps.iter().for_each(|s| println!("{:?}", s));
+
     unimplemented!()
+}
+
+#[derive(Debug)]
+struct Instruction(char, char);
+
+impl FromStr for Instruction {
+    type Err = std::char::ParseCharError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref RE: Regex =
+                Regex::new(r"Step ([A-Z]) must be finished before step ([A-Z]) can begin.")
+                    .unwrap();
+        }
+        let caps = RE.captures(s).unwrap();
+
+        Ok(Instruction(caps[1].parse()?, caps[2].parse()?))
+    }
 }
 
 #[cfg(test)]
