f1d57ef35fbf9dac43618077a9f3277eaac97966
Matthew Klein <mklein994@gmail.com>
2018-12-14 17:43:03 -0600

    Parse settings for day 9

--- src/bin/9.rs
+++ src/bin/9.rs
@@ -1,3 +1,5 @@
+use lazy_static::lazy_static;
+use regex::Regex;
 use std::str::FromStr;
 
 fn main() {
@@ -11,6 +13,7 @@ fn main() {
 
 fn part1(input: &str) -> u32 {
     let settings = Settings::from_str(input).unwrap();
+    println!("{:?}", settings);
     unimplemented!()
 }
 
@@ -24,7 +27,19 @@ impl FromStr for Settings {
     type Err = std::num::ParseIntError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        unimplemented!()
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"(?P<count>[0-9]+) players; last marble is worth (?P<points>[0-9]+) points"
+            )
+            .unwrap();
+        }
+
+        let caps = RE.captures(s).unwrap();
+
+        Ok(Self {
+            player_count: caps["count"].parse()?,
+            last_marble: caps["points"].parse()?,
+        })
     }
 }
 
