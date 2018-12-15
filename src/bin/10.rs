e7ceee8280845b8a0d51742daaca1f423af01e9e
Matthew Klein <mklein994@gmail.com>
2018-12-14 21:52:35 -0600

    Setup structs for day 10 part 1

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,3 +1,7 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+use std::str::FromStr;
+
 fn main() {
     let input = aoc2018::read_file(10);
     println!("{}", input);
@@ -7,4 +11,27 @@ fn main() {
 
 fn part1(input: &str) {
     unimplemented!();
+}
+
+#[derive(Debug)]
+struct Coordinate {
+    x: i32,
+    y: i32,
+}
+
+type Coord = Coordinate;
+type Velocity = Coordinate;
+
+#[derive(Debug)]
+struct Point {
+    coord: Coord,
+    velocity: Velocity,
+}
+
+impl FromStr for Point {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        unimplemented!()
+    }
 }
