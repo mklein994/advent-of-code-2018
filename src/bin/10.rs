288687167e3c66c9f1e6178ea33b8aa28b9d6955
Matthew Klein <mklein994@gmail.com>
2018-12-14 22:15:17 -0600

    Use example input for day 10 bin

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -4,6 +4,38 @@ use std::str::FromStr;
 
 fn main() {
     let input = aoc2018::read_file(10);
+    let input = "\
+position=< 9,  1> velocity=< 0,  2>
+position=< 7,  0> velocity=<-1,  0>
+position=< 3, -2> velocity=<-1,  1>
+position=< 6, 10> velocity=<-2, -1>
+position=< 2, -4> velocity=< 2,  2>
+position=<-6, 10> velocity=< 2, -2>
+position=< 1,  8> velocity=< 1, -1>
+position=< 1,  7> velocity=< 1,  0>
+position=<-3, 11> velocity=< 1, -2>
+position=< 7,  6> velocity=<-1, -1>
+position=<-2,  3> velocity=< 1,  0>
+position=<-4,  3> velocity=< 2,  0>
+position=<10, -3> velocity=<-1,  1>
+position=< 5, 11> velocity=< 1, -2>
+position=< 4,  7> velocity=< 0, -1>
+position=< 8, -2> velocity=< 0,  1>
+position=<15,  0> velocity=<-2,  0>
+position=< 1,  6> velocity=< 1,  0>
+position=< 8,  9> velocity=< 0, -1>
+position=< 3,  3> velocity=<-1,  1>
+position=< 0,  5> velocity=< 0, -1>
+position=<-2,  2> velocity=< 2,  0>
+position=< 5, -2> velocity=< 1,  2>
+position=< 1,  4> velocity=< 2,  1>
+position=<-2,  7> velocity=< 2, -2>
+position=< 3,  6> velocity=<-1, -1>
+position=< 5,  0> velocity=< 1,  0>
+position=<-6,  0> velocity=< 2,  0>
+position=< 5,  9> velocity=< 1, -2>
+position=<14,  7> velocity=<-2,  0>
+position=<-3,  6> velocity=< 2, -1>";
     // println!("{}", input);
 
     part1(&input).unwrap();
@@ -12,7 +44,6 @@ fn main() {
 fn part1(input: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut points: Vec<Point> = Vec::with_capacity(input.lines().count());
     for line in input.lines() {
-        // println!("{}", line);
         points.push(line.parse::<Point>()?);
     }
 
