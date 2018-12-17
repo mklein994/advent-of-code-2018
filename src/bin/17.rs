3b7b44ffd274324b969e129279b82ae6cacce8e1
Matthew Klein <mklein994@gmail.com>
2018-12-17 13:28:50 -0600

    Start creating ClayPiece struct for day 17

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -1,4 +1,23 @@
+use std::str::FromStr;
+use regex::Regex;
+use lazy_static::lazy_static;
+use std::ops::Range;
+
 fn main() {
     let input = aoc2018::read_file(17);
     println!("{}", input);
+}
+
+#[derive(Debug)]
+struct ClayPiece {
+    x: Range<u32>,
+    y: Range<u32>,
+}
+
+impl FromStr for ClayPiece {
+    type Err = std::num::ParseIntError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        unimplemented!()
+    }
 }
