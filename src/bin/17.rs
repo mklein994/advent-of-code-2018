d819b46723214c865bf24963bee153097b4069be
Matthew Klein <mklein994@gmail.com>
2018-12-18 14:41:01 -0600

    Move functions inside a Clay(Vec<ClayPiece>) struct

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -28,21 +28,88 @@ fn part1(input: &str) -> Result<u32, Box<dyn std::error::Error>> {
     for line in input.lines() {
         clay_pieces.push(line.parse()?);
     }
-    let bounds = get_bounds(&clay_pieces);
 
-    // Normalize the pieces to fit within the bounds.
-    clay_pieces.iter_mut().for_each(|p| {
-        p.x1 -= bounds.left;
-        p.x2 -= bounds.left;
-        p.y1 -= bounds.top;
-        p.y2 -= bounds.top;
-    });
+    let clay = Clay::new(clay_pieces);
 
     // clay_pieces.iter().for_each(|p| println!("{:?}", p));
 
-    draw_grid(&clay_pieces, &bounds);
+    clay.draw_grid();
 
     Ok(0)
+}
+
+#[derive(Debug)]
+struct Clay {
+    pieces: Vec<ClayPiece>,
+    bounds: Bounds,
+}
+
+impl Clay {
+    fn new(mut pieces: Vec<ClayPiece>) -> Self {
+        let bounds = Self::get_bounds(&pieces);
+
+        // Normalize the pieces to fit within the bounds.
+        pieces.iter_mut().for_each(|p| {
+            p.x1 -= bounds.left;
+            p.x2 -= bounds.left;
+            p.y1 -= bounds.top;
+            p.y2 -= bounds.top;
+        });
+
+        Self { pieces, bounds }
+    }
+
+    fn get_bounds(pieces: &[ClayPiece]) -> Bounds {
+        let initial_bounds = Bounds {
+            // The water spring starts at (0, 500), so the top must be 0. The x coordinates will be
+            // normalized to fit within the bounds.
+            top: WATER_SPRING.0 as u32,
+            bottom: pieces[0].y2,
+            left: pieces[0].x1,
+            right: pieces[0].x2,
+        };
+
+        let bounds = pieces.iter().fold(initial_bounds, |mut b, p| {
+            b.top = cmp::min(b.top, p.y1);
+            b.bottom = cmp::max(b.bottom, p.y2);
+            b.left = cmp::min(b.left, p.x1);
+            b.right = cmp::max(b.right, p.x2);
+            b
+        });
+        assert!(
+            bounds.left <= 500 && bounds.right >= 500,
+            "water from spring does not fall on clay pieces"
+        );
+        bounds
+    }
+
+    fn draw_grid(&self) {
+        let Clay { pieces, bounds } = self;
+        println!("{:?}", bounds);
+
+        // Fill the grid with sand (`.`) pieces.
+        let mut grid = vec![vec![b'.'; bounds.width() + 1]; bounds.height() + 1];
+
+        // Add the water spring to the grid.
+        grid[0][WATER_SPRING.1 - 1 - bounds.left as usize] = b'+';
+
+        for piece in pieces {
+            for row in piece.y1..=piece.y2 {
+                for column in piece.x1..=piece.x2 {
+                    // Insert a piece of clay into the grid.
+                    grid[row as usize][column as usize] = b'#';
+                }
+            }
+        }
+
+        let mut grid_string = String::new();
+        for row in grid {
+            grid_string.push_str(std::str::from_utf8(&row).unwrap());
+            grid_string.push('\n');
+        }
+
+        println!("{}", grid_string);
+    }
 }
 
 #[derive(Debug)]
@@ -61,57 +128,6 @@ impl Bounds {
     fn height(&self) -> usize {
         (self.bottom - self.top) as usize
     }
-}
-
-fn draw_grid(pieces: &[ClayPiece], bounds: &Bounds) {
-    println!("{:?}", bounds);
-
-    // Fill the grid with sand (`.`) pieces.
-    let mut grid = vec![vec![b'.'; bounds.width() + 1]; bounds.height() + 1];
-
-    // Add the water spring to the grid.
-    grid[0][WATER_SPRING.1 - 1 - bounds.left as usize] = b'+';
-
-    for piece in pieces {
-        for row in piece.y1..=piece.y2 {
-            for column in piece.x1..=piece.x2 {
-                // Insert a piece of clay into the grid.
-                grid[row as usize][column as usize] = b'#';
-            }
-        }
-    }
-
-    let mut grid_string = String::new();
-    for row in grid {
-        grid_string.push_str(std::str::from_utf8(&row).unwrap());
-        grid_string.push('\n');
-    }
-
-    println!("{}", grid_string);
-}
-
-fn get_bounds(pieces: &[ClayPiece]) -> Bounds {
-    let initial_bounds = Bounds {
-        // The water spring starts at (0, 500), so the top must be 0. The x coordinates will be
-        // normalized to fit within the bounds.
-        top: WATER_SPRING.0 as u32,
-        bottom: pieces[0].y2,
-        left: pieces[0].x1,
-        right: pieces[0].x2,
-    };
-
-    let bounds = pieces.iter().fold(initial_bounds, |mut b, p| {
-        b.top = cmp::min(b.top, p.y1);
-        b.bottom = cmp::max(b.bottom, p.y2);
-        b.left = cmp::min(b.left, p.x1);
-        b.right = cmp::max(b.right, p.x2);
-        b
-    });
-    assert!(
-        bounds.left <= 500 && bounds.right >= 500,
-        "water from spring does not fall on clay pieces"
-    );
-    bounds
 }
 
 #[derive(Debug, Default)]
