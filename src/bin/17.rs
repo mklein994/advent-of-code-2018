8329598ab931309b015c4f8a35b3004bb2090201
Matthew Klein <mklein994@gmail.com>
2018-12-18 23:28:01 -0600

    Use example input for day 17 bin

--- src/bin/17.rs
+++ src/bin/17.rs
@@ -5,6 +5,16 @@ use std::str::FromStr;
 
 fn main() {
     let input = aoc2018::read_file(17);
+    let input = "\
+x=495, y=2..7
+y=7, x=495..501
+x=501, y=3..7
+x=498, y=2..4
+x=506, y=1..2
+x=498, y=10..13
+x=504, y=10..13
+y=13, x=498..504";
+
     // println!("{}", input);
 
     let area = part1(&input).unwrap();
