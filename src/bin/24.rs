d9eb00bb8d2568ed3dd4469b878b65944e1b3796
Matthew Klein <mklein994@gmail.com>
2018-12-25 02:14:19 -0600

    Create an Armies struct to handle Vec<Group> operations

--- src/bin/24.rs
+++ src/bin/24.rs
@@ -25,15 +25,27 @@ cold) with an attack that does 12 slashing damage at initiative 4";
 }
 
 fn part1(input: &str) -> Result<usize, Box<Error>> {
-    let groups = parse_groups(&input)?;
-    groups.iter().for_each(|g| println!("{:?}", g));
+    let armies = Armies::parse_groups(&input)?;
+    armies.groups.iter().for_each(|g| println!("{:?}", g));
     Ok(0)
 }
 
-fn parse_groups(input: &str) -> Result<Vec<Group>, Box<Error>> {
-    lazy_static! {
-        static ref RE: Regex = Regex::new(
-            r"(?x)
+#[derive(Debug)]
+struct Armies {
+    groups: Vec<Group>,
+}
+
+impl From<Vec<Group>> for Armies {
+    fn from(groups: Vec<Group>) -> Self {
+        Self { groups }
+    }
+}
+
+impl Armies {
+    fn parse_groups(input: &str) -> Result<Self, Box<Error>> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"(?x)
             (?P<unit_count>\d+)(?-x: units each with )
             (?P<hit_points>\d+)(?-x: hit points )
             (?:\(
@@ -44,101 +56,102 @@ fn parse_groups(input: &str) -> Result<Vec<Group>, Box<Error>> {
             (?-x:with an attack that does )
             (?P<damage>\d+)\ (?P<attack_kind>\w+)(?-x: damage at initiative )
             (?P<initiative>\d+)"
-        )
-        .unwrap();
+            )
+            .unwrap();
+        }
+
+        let mut groups: Vec<Group> = vec![];
+
+        // Skip the heading, and stop at the next empty line.
+        for line in input.lines().skip(1).take_while(|l| !l.is_empty()) {
+            let allegiance = Allegiance::ImmuneSystem;
+            let caps = RE
+                .captures(line)
+                .expect("unknown immune system group captures");
+
+            let mut group = Group {
+                allegiance,
+                unit_count: caps["unit_count"].parse()?,
+                initiative: caps["initiative"].parse()?,
+                attack_damage: caps["damage"].parse()?,
+                attack_kind: AttackKind(caps["attack_kind"].to_string()),
+                ..Default::default()
+            };
+
+            if let Some(defense) = caps.name("defense_kind") {
+                let defenses = caps["defense"]
+                    .trim_matches(char::is_whitespace)
+                    .split(',')
+                    .map(|d| AttackKind(d.to_string()))
+                    .collect::<Vec<_>>();
+                match defense.as_str() {
+                    "weak" => group.weaknesses = defenses,
+                    "immune" => group.immunities = defenses,
+                    _ => panic!("unknown defense kind"),
+                };
+            }
+
+            if let Some(defense) = caps.name("other_defense_kind") {
+                let defenses = caps["other_defense"]
+                    .trim_matches(char::is_whitespace)
+                    .split(',')
+                    .map(|d| AttackKind(d.to_string()))
+                    .collect::<Vec<_>>();
+                match defense.as_str() {
+                    "weak" => group.weaknesses = defenses,
+                    "immune" => group.immunities = defenses,
+                    _ => panic!("unknown defense kind"),
+                };
+            }
+
+            groups.push(group);
+        }
+
+        // Skip until the next section, then skip the blank line and the heading.
+        for line in input.lines().skip_while(|l| !l.is_empty()).skip(2) {
+            let allegiance = Allegiance::Infection;
+            let caps = RE.captures(line).expect("unknown infection group captures");
+
+            let mut group = Group {
+                allegiance,
+                unit_count: caps["unit_count"].parse()?,
+                initiative: caps["initiative"].parse()?,
+                attack_damage: caps["damage"].parse()?,
+                attack_kind: AttackKind(caps["attack_kind"].to_string()),
+                ..Default::default()
+            };
+
+            if let Some(defense) = caps.name("defense_kind") {
+                let defenses = caps["defense"]
+                    .trim_matches(char::is_whitespace)
+                    .split(',')
+                    .map(|d| AttackKind(d.to_string()))
+                    .collect::<Vec<_>>();
+                match defense.as_str() {
+                    "weak" => group.weaknesses = defenses,
+                    "immune" => group.immunities = defenses,
+                    _ => panic!("unknown defense kind"),
+                };
+            }
+
+            if let Some(defense) = caps.name("other_defense_kind") {
+                let defenses = caps["other_defense"]
+                    .trim_matches(char::is_whitespace)
+                    .split(',')
+                    .map(|d| AttackKind(d.to_string()))
+                    .collect::<Vec<_>>();
+                match defense.as_str() {
+                    "weak" => group.weaknesses = defenses,
+                    "immune" => group.immunities = defenses,
+                    _ => panic!("unknown defense kind"),
+                };
+            }
+
+            groups.push(group);
+        }
+
+        Ok(Armies { groups })
     }
-
-    let mut groups: Vec<Group> = vec![];
-
-    // Skip the heading, and stop at the next empty line.
-    for line in input.lines().skip(1).take_while(|l| !l.is_empty()) {
-        let allegiance = Allegiance::ImmuneSystem;
-        let caps = RE
-            .captures(line)
-            .expect("unknown immune system group captures");
-
-        let mut group = Group {
-            allegiance,
-            unit_count: caps["unit_count"].parse()?,
-            initiative: caps["initiative"].parse()?,
-            attack_damage: caps["damage"].parse()?,
-            attack_kind: AttackKind(caps["attack_kind"].to_string()),
-            ..Default::default()
-        };
-
-        if let Some(defense) = caps.name("defense_kind") {
-            let defenses = caps["defense"]
-                .trim_matches(char::is_whitespace)
-                .split(',')
-                .map(|d| AttackKind(d.to_string()))
-                .collect::<Vec<_>>();
-            match defense.as_str() {
-                "weak" => group.weaknesses = defenses,
-                "immune" => group.immunities = defenses,
-                _ => panic!("unknown defense kind"),
-            };
-        }
-
-        if let Some(defense) = caps.name("other_defense_kind") {
-            let defenses = caps["other_defense"]
-                .trim_matches(char::is_whitespace)
-                .split(',')
-                .map(|d| AttackKind(d.to_string()))
-                .collect::<Vec<_>>();
-            match defense.as_str() {
-                "weak" => group.weaknesses = defenses,
-                "immune" => group.immunities = defenses,
-                _ => panic!("unknown defense kind"),
-            };
-        }
-
-        groups.push(group);
-    }
-
-    // Skip until the next section, then skip the blank line and the heading.
-    for line in input.lines().skip_while(|l| !l.is_empty()).skip(2) {
-        let allegiance = Allegiance::Infection;
-        let caps = RE.captures(line).expect("unknown infection group captures");
-
-        let mut group = Group {
-            allegiance,
-            unit_count: caps["unit_count"].parse()?,
-            initiative: caps["initiative"].parse()?,
-            attack_damage: caps["damage"].parse()?,
-            attack_kind: AttackKind(caps["attack_kind"].to_string()),
-            ..Default::default()
-        };
-
-        if let Some(defense) = caps.name("defense_kind") {
-            let defenses = caps["defense"]
-                .trim_matches(char::is_whitespace)
-                .split(',')
-                .map(|d| AttackKind(d.to_string()))
-                .collect::<Vec<_>>();
-            match defense.as_str() {
-                "weak" => group.weaknesses = defenses,
-                "immune" => group.immunities = defenses,
-                _ => panic!("unknown defense kind"),
-            };
-        }
-
-        if let Some(defense) = caps.name("other_defense_kind") {
-            let defenses = caps["other_defense"]
-                .trim_matches(char::is_whitespace)
-                .split(',')
-                .map(|d| AttackKind(d.to_string()))
-                .collect::<Vec<_>>();
-            match defense.as_str() {
-                "weak" => group.weaknesses = defenses,
-                "immune" => group.immunities = defenses,
-                _ => panic!("unknown defense kind"),
-            };
-        }
-
-        groups.push(group);
-    }
-
-    Ok(groups)
 }
 
 #[derive(Debug)]
